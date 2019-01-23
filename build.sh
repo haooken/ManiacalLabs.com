247402ad8e0e1d234be382b2c14120939dcbebfb
Adam Haile <adammhaile@gmail.com>
2019-01-22 19:41:41 -0500

    Testing stage deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
+set -o xtrace
+
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" || $TRAVIS_BRANCH != 'master' ]]; then
+  echo "Modifying for stage deploy"
   sed -i 's/ManiacalLabs.com/stage.ManiacalLabs.com/g' $TRAVIS_BUILD_DIR/config.yaml
 fi
 
 chmod +x $TRAVIS_BUILD_DIR/bin/hugo
 $TRAVIS_BUILD_DIR/bin/hugo
+
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" || $TRAVIS_BRANCH != 'master' ]]; then
+  echo "Syncing to stage server"
+  rsync -rav --delete $TRAVIS_BUILD_DIR/public/ cyrberus@stage.maniacallabs.com:/home/cyrberus/stage.maniacallabs.com/
+fi
