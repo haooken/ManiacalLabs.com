c9671848f1dc03c94d6b677ef43842bf20ae1747
Adam Haile <adammhaile@gmail.com>
2019-02-26 08:25:18 -0500

    Update hugo for future builds

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o xtrace
+
+chmod +x $TRAVIS_BUILD_DIR/bin/hugo
 
 if [[ "$TRAVIS_PULL_REQUEST_BRANCH" || $TRAVIS_BRANCH != 'master' ]]; then
   echo "Modifying for stage deploy"
   sed -i 's/ManiacalLabs.com/stage.ManiacalLabs.com/g' $TRAVIS_BUILD_DIR/config.yaml
-fi
-
-chmod +x $TRAVIS_BUILD_DIR/bin/hugo
-$TRAVIS_BUILD_DIR/bin/hugo
-
-if [[ "$TRAVIS_PULL_REQUEST_BRANCH" || $TRAVIS_BRANCH != 'master' ]]; then
+  echo "Running hugo with future enabled"
+  $TRAVIS_BUILD_DIR/bin/hugo -F
   echo "Syncing to stage server"
   rsync -rav --delete $TRAVIS_BUILD_DIR/public/ cyrberus@stage.maniacallabs.com:/home/cyrberus/stage.maniacallabs.com/
+else
+  echo "Running hugo for prod deploy"
+  $TRAVIS_BUILD_DIR/bin/hugo
 fi
